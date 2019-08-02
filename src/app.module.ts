ad26b8bc79b13f0268bac5800e7ab6797cded8d6
Calm Andy <thought7878@qq.com>
2019-08-02 14:48:03 +0800

    数据库连接配置

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsModule } from './modules/posts/posts.module';
@@ -6,9 +7,23 @@ import { DemoMiddleware } from './core/middlewares/demo.middleware';
 import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 import { DemoRolesGuard } from './core/guards/demo-roles.guard';
 import { LoggingInterceptor } from './core/interceptors/logging.interceptor';
+import { PostModule } from './modules/post/post.module';
 
 @Module({
-  imports: [PostsModule],
+  imports: [
+    PostsModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'nest',
+      password: 'password',
+      database: 'nest',
+      synchronize: true,
+      entities: [__dirname + '/**/*.entity.ts'],
+    }),
+    PostModule,
+  ],
   controllers: [AppController],
   providers: [
     AppService,
