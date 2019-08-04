178eb9b57c355342e549b5d72d9b71394644485a
Calm Andy <thought7878@qq.com>
2019-08-04 08:56:01 +0800

    查询实体全部数据 查询实体单个数据 更新单个实体数据 删除实体数据

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -1,4 +1,36 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  Param,
+  Put,
+  Delete,
+} from '@nestjs/common';
+import { PostService } from './post.service';
 
 @Controller('post')
-export class PostController {}
+export class PostController {
+  constructor(private readonly postService: PostService) {}
+
+  @Post()
+  async insert(@Body() data) {
+    return this.postService.insert(data);
+  }
+  @Get()
+  async find() {
+    return await this.postService.find();
+  }
+  @Get(':id')
+  async findById(@Param('id') id: string) {
+    return await this.postService.findById(id);
+  }
+  @Put(':id')
+  async update(@Param('id') id: string, @Body() data) {
+    return await this.postService.update(id, data);
+  }
+  @Delete(':id')
+  async delete(@Param('id') id: string) {
+    return await this.postService.delete(id);
+  }
+}
--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -1,4 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Post } from './post.entity';
 
 @Injectable()
-export class PostService {}
+export class PostService {
+  constructor(
+    @InjectRepository(Post)
+    private readonly postRepository: Repository<Post>,
+  ) {}
+
+  async insert(data) {
+    const entity = this.postRepository.create(data);
+    await this.postRepository.save(entity);
+    return entity;
+  }
+
+  async find() {
+    return await this.postRepository.find();
+  }
+
+  async findById(id: string) {
+    const post = await this.postRepository.findOne(id);
+    return post;
+  }
+
+  async update(id: string, data) {
+    const result = await this.postRepository.update(id, data);
+    return result;
+  }
+  async delete(id: string) {
+    const result = await this.postRepository.delete(id);
+    return result;
+  }
+}
